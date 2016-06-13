e70bf61f61413f70073c7392f93fcd20ad62a3c8
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-06-13 10:30:15 +0200

    Fixed release script

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./gradlew clean build && ./gradlew uploadArchives && ./gradlew closeRepository && ./gradlew promoteRepository && ./gradlew pushRelease
+./gradlew clean build && ./gradlew uploadArchives && ./gradlew closeRepository && ./gradlew promoteRepository
